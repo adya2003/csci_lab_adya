6ee8b30b113ec937a97357004748849900f5f486
adya2003 <ag573@uowmail.edu.au>
2024-09-27 12:29:40 +0400

    Add files via upload

---
+++ lab 2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BEANS_REQ 8
+#define WATER_ESPRESSO 30
+#define WATER_CAPPUCINO 70
+#define WATER_MOCHA 160  
+#define MILK_MOCHA 160
+#define CHOCOLATE_SYRUP_MOCHA 30 
+#define THRESHOLD_BEANS 10
+#define THRESHOLD_WATER 50
+#define THRESHOLD_MILK 100
+#define THRESHOLD_SYRUP 20
+
+#define ADMIN_PASSWORD "1234"
+
+float_total_sale = 0.0;
+int coffe_beans = 100, water = 100, milk = 300, chocolate_syrup = 100;
+float espresso_price = 3.5;
+float cappucino_price = 4.5;
+float  macha_price = 5.5;
